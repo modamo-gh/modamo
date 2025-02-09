d8d31374835860fab21fe265b922ce44e177aa5f
modamo-gh <morganthemosaic@gmail.com>
2025-02-09 15:55:29 -0500

    Collapse segments

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import { motion } from "motion/react";
+import { useState } from "react";
 
 const containerVariants = {
-	hidden: { opacity: 0 },
+	collapse: {
+		transition: {
+			duration: 1
+		}
+	},
 	show: {
 		opacity: 1,
 		transition: {
@@ -13,6 +18,13 @@ const containerVariants = {
 };
 
 const itemVariants = {
+	collapse: {
+		opacity: 1,
+		scale: 0,
+		transition: { duration: 1 },
+		x: 0,
+		y: 0
+	},
 	hidden: { opacity: 0 },
 	show: {
 		opacity: 1,
@@ -23,11 +35,17 @@ const itemVariants = {
 };
 
 const Home = () => {
+	const [collapse, setCollapse] = useState(false);
+
 	return (
 		<main className="bg-fuchsia-700 relative h-screen w-screen">
 			<motion.div
-				animate="show"
+				animate={collapse ? "collapse" : "show"}
+				id="segments"
 				initial="hidden"
+				onAnimationComplete={() => {
+					setCollapse(true);
+				}}
 				variants={containerVariants}
 			>
 				<motion.div
