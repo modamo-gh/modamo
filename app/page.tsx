445613cebc2bb202ff8f54310e15d904d7a07e7a
modamo-gh <morganthemosaic@gmail.com>
2025-02-09 15:39:49 -0500

    Animate segments

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,51 @@
 
 import { motion } from "motion/react";
 
+const containerVariants = {
+	hidden: { opacity: 0 },
+	show: {
+		opacity: 1,
+		transition: {
+			staggerChildren: 1
+		}
+	}
+};
+
+const itemVariants = {
+	hidden: { opacity: 0 },
+	show: {
+		opacity: 1,
+		transition: {
+			duration: 1
+		}
+	}
+};
+
 const Home = () => {
 	return (
 		<main className="bg-fuchsia-700 relative h-screen w-screen">
-			<div className="absolute clip-triangle-top inset-0"></div>
-			<div className="absolute clip-triangle-right inset-0"></div>
-			<div className="absolute clip-triangle-bottom inset-0"></div>
-			<div className="absolute clip-triangle-left inset-0"></div>
+			<motion.div
+				animate="show"
+				initial="hidden"
+				variants={containerVariants}
+			>
+				<motion.div
+					className="absolute clip-triangle-top inset-0"
+					variants={itemVariants}
+				></motion.div>
+				<motion.div
+					className="absolute clip-triangle-right inset-0"
+					variants={itemVariants}
+				></motion.div>
+				<motion.div
+					className="absolute clip-triangle-bottom inset-0"
+					variants={itemVariants}
+				></motion.div>
+				<motion.div
+					className="absolute clip-triangle-left inset-0"
+					variants={itemVariants}
+				></motion.div>
+			</motion.div>
 		</main>
 	);
 };
