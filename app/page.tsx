8f6bba7dbf2fe0a2131f91f13d58af03d34fb20c
modamo-gh <morganthemosaic@gmail.com>
2025-02-09 15:29:37 -0500

    Add placeholder images

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,10 @@ import { motion } from "motion/react";
 const Home = () => {
 	return (
 		<main className="bg-fuchsia-700 relative h-screen w-screen">
-			<div className="absolute bg-red-500 clip-triangle-top inset-0">
-				<p></p>
-			</div>
-			<div className="absolute bg-yellow-500 clip-triangle-right inset-0">
-				<p></p>
-			</div>
-			<div className="absolute bg-green-500 clip-triangle-bottom inset-0">
-				<p></p>
-			</div>
-			<div className="absolute bg-blue-500 clip-triangle-left inset-0">
-				<p></p>
-			</div>
+			<div className="absolute clip-triangle-top inset-0"></div>
+			<div className="absolute clip-triangle-right inset-0"></div>
+			<div className="absolute clip-triangle-bottom inset-0"></div>
+			<div className="absolute clip-triangle-left inset-0"></div>
 		</main>
 	);
 };
