9cdca34286d0c00df2f7f8a8c411547ec7c0d484
modamo-gh <morganthemosaic@gmail.com>
2025-02-26 13:11:39 -0500

    Implement grid for portfolio

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,12 +2,12 @@ import Navbar from "@/components/Navbar";
 
 const Portfolio = () => {
 	return (
-		<main className="bg-gradient-to-br from-rose-600 h-screen relative to-purple-600 via-fuchsia-600 w-screen">
+		<main className="bg-gradient-to-br flex flex-col from-rose-600 h-screen relative to-purple-600 via-fuchsia-600 w-screen">
 			<Navbar />
-			<div className="bg-cyan-500 grid grid-cols-3 gap-4 p-4 flex-1 h-96">
-				<div className="bg-white h-40 w-full"></div>
-				<div className="bg-white h-40 w-full"></div>
-				<div className="bg-white h-40 w-full"></div>
+			<div className="bg-cyan-500 grid grid-cols-3 gap-4 p-4 flex-1 overflow-y-auto overflow-x-hidden">
+				{Array.from({ length: 12 }).map((_, index) => (
+					<div key={index} className="bg-white h-80"></div>
+				))}
 			</div>
 		</main>
 	);
